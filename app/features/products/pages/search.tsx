33dcc2e0d1c9c33db15f8c031df84c3078ade908
minungHan <hmu332233@gmail.com>
2025-07-27 17:52:56 +0900

    3.20 Search Page

--- app/features/products/pages/search.tsx
+++ app/features/products/pages/search.tsx
@@ -1,13 +1,36 @@
-import { Link, type MetaFunction } from "react-router";
+import { data, Form, Link, type MetaFunction } from "react-router";
 import type { Route } from "./+types/search";
+import { z } from "zod";
+import { Hero } from "~/common/components/hero";
+import ProductPagination from "~/common/components/productPagination";
+import { ProductCard } from "../components/productCard";
+import { Input } from "~/common/components/ui/input";
+import { Button } from "~/common/components/ui/button";
+
+const paramsSchema = z.object({
+  query: z.string().optional().default(""),
+  page: z.coerce.number().optional().default(1),
+});
 
 export function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
-  const query = url.searchParams.get("q") || "";
-  
+  const { success, data: parsedData } = paramsSchema.safeParse(
+    Object.fromEntries(url.searchParams)
+  );
+
+  if (!success) {
+    throw data(
+      { error_code: 'invalid_params', message: 'Invalid parameters' },
+      { status: 400 }
+    );
+  }
+
+  const { query, page } = parsedData;
+
   return {
     products: [],
-    query
+    query,
+    page
   };
 }
 
@@ -26,8 +49,35 @@ export default function ProductSearchPage({ loaderData, actionData }: Route.Comp
   const { products, query } = loaderData;
   
   return (
-    <div>
-      <h1>Search Products: {query}</h1>
+    <div className="space-y-10">
+      <Hero title="Search Products" />
+      <Form>
+        <Input
+          name="query"
+          placeholder="Search for products..."
+          defaultValue={query}
+        />
+        <Button type="submit" className="mt-4">
+          Search
+        </Button>
+      </Form>
+      <div className="flex flex-col w-full gap-5 max-w-screen-md mx-auto">
+            {Array.from({ length: 10 }).map((_, index) => (
+              <ProductCard
+                key={index}
+                id={`productId-${index}`}
+                title={`Product Title ${index + 1}`}
+                description="Product description goes here. It should be short and concise."
+                commentCount={2}
+                viewCount={5}
+                upvoteCount={120}
+              />
+            ))}
+            </div>
+            <ProductPagination 
+              totalPage={5}
+              onPageChange={(page) => console.log(page)} 
+            />
     </div>
   );
 }
